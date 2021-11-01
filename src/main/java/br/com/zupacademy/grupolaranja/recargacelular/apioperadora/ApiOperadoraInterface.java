fe48ee6427b093a3ad63688132726e93f19c9025
Everton-ZUP <everton.fabris@zup.com.br>
2021-11-01 17:04:34 -0300

    conexão com api da operadora

---
+++ src/main/java/br/com/zupacademy/grupolaranja/recargacelular/apioperadora/ApiOperadoraInterface.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.grupolaranja.recargacelular.apioperadora;
+
+import br.com.zupacademy.grupolaranja.recargacelular.apioperadora.dto.OperadoraResponse;
+import br.com.zupacademy.grupolaranja.recargacelular.apioperadora.dto.RecarregarRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(url = "${feign.client.operadora.url}", name = "ApiOperadora")
+public interface ApiOperadoraInterface {
+    @PostMapping
+    OperadoraResponse recarregarCelular(@RequestBody RecarregarRequest request);
+}
---
+++ src/main/java/br/com/zupacademy/grupolaranja/recargacelular/apioperadora/dto/OperadoraResponse.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.grupolaranja.recargacelular.apioperadora.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.math.BigDecimal;
+
+public class OperadoraResponse {
+
+    @JsonProperty
+    private String numero;
+    @JsonProperty
+    private BigDecimal valor;
+
+    public OperadoraResponse(String numero, BigDecimal valor) {
+        this.numero = numero;
+        this.valor = valor;
+    }
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
