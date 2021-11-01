741ae5ad3eb8d68078ed136fe5e9fe18ccd776a4
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-11-01 11:52:19 -0300

    criando repositorio inicial recarga de celular

---
+++ src/main/java/br/com/zupacademy/grupolaranja/recargacelular/RecargacelularApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.grupolaranja.recargacelular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecargacelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecargacelularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/grupolaranja/recargacelular/RecargacelularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.grupolaranja.recargacelular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecargacelularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
