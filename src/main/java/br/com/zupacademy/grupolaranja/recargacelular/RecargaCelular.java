f1f79a63bbaf4c84a759df4146c08ffdd84a019e
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-11-01 16:35:32 -0300

    Remodelando a Entidade e adequando o controller

--- src/main/java/br/com/zupacademy/grupolaranja/recargacelular/RecargaCelular.java
+++ src/main/java/br/com/zupacademy/grupolaranja/recargacelular/RecargaCelular.java
@@ -3,6 +3,7 @@ package br.com.zupacademy.grupolaranja.recargacelular;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 public class RecargaCelular {
@@ -11,12 +12,46 @@ public class RecargaCelular {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
-    
+    @Column(nullable = false, unique = true)
     private String numeroCelular;
+    
+    @Column(nullable = false, updatable = false)
     private Enum<Operadora> operadoraEnum;
+    
+    @Column(nullable = false, updatable = false)
     private BigDecimal valorRecarga;
+    
+    private LocalDateTime criadoEm;
     
     
     public RecargaCelular(RecargaCelularRequest request) {
+        this.numeroCelular = request.getNumeroCelular();
+        this.operadoraEnum = request.getOperadoraEnum();
+        this.valorRecarga = request.getValorRecarga();
+        this.criadoEm = LocalDateTime.now();
+    }
+    
+    @Deprecated
+    public RecargaCelular() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+    
+    public Enum<Operadora> getOperadoraEnum() {
+        return operadoraEnum;
+    }
+    
+    public BigDecimal getValorRecarga() {
+        return valorRecarga;
+    }
+    
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
     }
 }
