602ee8be9bcc20f0d7d55709a15959e59ce30658
Everton-ZUP <everton.fabris@zup.com.br>
2021-11-03 10:20:25 -0300

    Adicionando banco de dados e feigh

--- src/main/java/br/com/zupacademy/grupolaranja/recargacelular/RecargacelularApplication.java
+++ src/main/java/br/com/zupacademy/grupolaranja/recargacelular/RecargacelularApplication.java
@@ -2,9 +2,11 @@ package br.com.zupacademy.grupolaranja.recargacelular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableFeignClients
 @EnableSwagger2
 public class RecargacelularApplication {
 	public static void main(String[] args) {
