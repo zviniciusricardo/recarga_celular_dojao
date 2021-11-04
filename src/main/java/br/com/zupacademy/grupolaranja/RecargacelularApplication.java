fabde94b0adb35a2be5c0660205eb76d65f92cdc
Everton-ZUP <everton.fabris@zup.com.br>
2021-11-04 17:18:53 -0300

    Teste de endpoint caminho feliz

--- src/main/java/br/com/zupacademy/grupolaranja/RecargacelularApplication.java
+++ src/main/java/br/com/zupacademy/grupolaranja/RecargacelularApplication.java
@@ -2,6 +2,7 @@ package br.com.zupacademy.grupolaranja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
--- src/test/java/br/com/zupacademy/grupolaranja/recargacelular/RecargaCelularControllerTest.java
+++ src/test/java/br/com/zupacademy/grupolaranja/recargacelular/RecargaCelularControllerTest.java
@@ -2,10 +2,13 @@ package br.com.zupacademy.grupolaranja.recargacelular;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@AutoConfigureWireMock(port = 8080)
 @ActiveProfiles("test")
 class RecargaCelularControllerTest {
 
@@ -29,13 +33,14 @@ class RecargaCelularControllerTest {
 
     @Test
     public void fazerUmaRecargaComSucesso() throws Exception {
-//        RecargaCelularRequest requestBody = new RecargaCelularRequest("123",
-//                "(46) 999990800",Operadora.VIVO,new BigDecimal("100"));
-//
-//        RequestBuilder builder = MockMvcRequestBuilders.post("/recarga-celulares")
-//                .content(new ObjectMapper().writeValueAsString(requestBody))
-//                .contentType(MediaType.APPLICATION_JSON);
-//
-//        mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
+        RecargaCelularRequest requestBody = new RecargaCelularRequest("123",
+                "(46) 999990800",Operadora.VIVO,new BigDecimal("100"));
+
+        System.out.println(new ObjectMapper().writeValueAsString(requestBody));
+        RequestBuilder builder = MockMvcRequestBuilders.post("/recarga-celulares")
+                .content(new ObjectMapper().writeValueAsString(requestBody))
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(builder).andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
